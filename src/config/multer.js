374a8ef0abbbdfe6594db1a5568c1ec3e0076464
Tállyto Rodrigues <rodrigues.tallyto@hotmail.com>
2019-12-19 23:08:31 -0400

    Fazendo upload na nuvem

--- src/config/multer.js
+++ src/config/multer.js
@@ -1,12 +1,11 @@
 const multer = require("multer");
 const path = require("path");
 const crypto = require("crypto");
-const multerS3 = require('multer-s3')
+const multerS3 = require("multer-s3");
+const aws = require("aws-sdk");
 
-
-module.exports = {
-  dest: path.resolve(__dirname, "..", "..", "public", "uploads"),
-  storage: multer.diskStorage({
+const storageTypes = {
+  local: multer.diskStorage({
     destination: (req, file, cb) => {
       cb(null, path.resolve(__dirname, "..", "..", "public", "uploads"));
     },
@@ -14,21 +13,39 @@ module.exports = {
       crypto.randomBytes(16, (err, hash) => {
         if (err) cb(err);
 
+        file.key = `${hash.toString("hex")}-${file.originalname}`;
+        cb(null, file.key);
+      });
+    }
+  }),
+  s3: multerS3({
+    s3: new aws.S3({
+      accessKeyId: "AKIAVHKUOQJ4DWASD7F5",
+      secretAccessKey: "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",
+      region: "sa-east-1"
+    }),
+    bucket: "upload-party",
+    contentType: multerS3.AUTO_CONTENT_TYPE,
+    acl: "public-read",
+    key: (req, file, cb) => {
+      crypto.randomBytes(16, (err, hash) => {
+        if (err) cb(err);
+
         const fileName = `${hash.toString("hex")}-${file.originalname}`;
         cb(null, fileName);
       });
     }
-  }),
+  })
+};
+
+module.exports = {
+  dest: path.resolve(__dirname, "..", "..", "public", "uploads"),
+  storage: storageTypes["s3"],
   limits: {
     fileSize: 2 * 1024 * 1024
   },
   fileFilter: (req, file, cb) => {
-    const allowedMimes = [
-      "image/jpeg",
-      "image/png",
-      "image/gif",
-      "image/jpg"
-    ];
+    const allowedMimes = ["image/jpeg", "image/png", "image/gif", "image/jpg"];
 
     if (allowedMimes.includes(file.mimetype)) {
       cb(null, true);
--- src/routes.js
+++ src/routes.js
@@ -7,11 +7,12 @@ const Post = require('./models/Post')
 
 routes.post("/posts" ,multer(multerConfig).single('file'), async ( req, res)=>{
 
+    const {originalname: name, size, key , location: url = ""} = req.file
     const post = await Post.create({
-        name: req.file.originalname,
-        size: req.file.size,
-        key:req.file.filename,
-        url: ''
+        name,
+        size,
+        key,
+        url,
     })
     console.log(req.file);
     res.json(post)
